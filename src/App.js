38fad83f232d257e418bc4edbb4e4ed6f81ab0aa
Remu Ollinen <remu.ollinen@gmail.com>
2021-11-22 14:32:11 +0200

    rendered countries with selected data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+	state = {
+		data: [],
+	};
+
+	componentDidMount() {
+		axios.get("https://restcountries.com/v3.1/all").then((response) => {
+			this.setState({ data: response.data });
+		});
+	}
+
 	render() {
-		return <div>Hello World!</div>;
+		return (
+			<main>
+				{this.state.data.map((country) => {
+					return (
+						<div key={country.name.common} className="country-card">
+							<h2>{country.name.common}</h2>
+							<img
+								src={`${country.flags.svg}`}
+								alt={`flag of ${country.name.common}`}
+							/>
+							<p>
+								Capital: <span>{country.capital}</span>
+							</p>
+							<p>
+								They drive on the <span>{country.car.side}</span> side of the
+								road.
+							</p>
+						</div>
+					);
+				})}
+			</main>
+		);
 	}
 }
 
