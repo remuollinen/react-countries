ca7be31b11da1ba69316b615e5e10679c1788cb7
Remu Ollinen <remu.ollinen@gmail.com>
2021-11-23 13:44:54 +0200

    added spinning loader

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 import Numeral from "react-numeral";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 class App extends Component {
 	state = {
 		data: [],
 		search: "",
+		isLoading: true,
 	};
 
 	componentDidMount() {
 		axios
 			.get(
-				"https://restcountries.com/v2/all?fields=name,capital,flags,population,languages"
+				"https://restcountries.com/v2/all?fields=name,capital,flags,population,languages,callingCodes"
 			)
 			.then((response) => {
-				this.setState({ data: response.data });
-				// console.log(this.state.data);
+				this.setState({ data: response.data, isLoading: false });
+				console.log(this.state.data);
 			});
 	}
 
@@ -42,34 +45,49 @@ class App extends Component {
 					/>
 				</header>
 				<main>
-					{filteredCountries.map((country) => {
-						return (
-							<div key={country.name} className="country-card">
-								<div className="card-header">
-									<h2>{country.name}</h2>
+					{this.state.isLoading ? (
+						<div>
+							<FontAwesomeIcon
+								className="fas fa-spinner fa-spin loader"
+								icon={faSpinner}
+							/>
+						</div>
+					) : (
+						filteredCountries.map((country) => {
+							return (
+								<div key={country.name} className="country-card">
+									<div className="card-header">
+										<h2>{country.name}</h2>
+										<p>
+											<span>{country.capital}</span>
+										</p>
+									</div>
+									<img
+										src={`${country.flags.svg}`}
+										alt={`flag of ${country.name}`}
+									/>
 									<p>
-										<span>{country.capital}</span>
+										Population:{" "}
+										<span>
+											{<Numeral value={country.population} format={"0.0a"} />}
+										</span>
+									</p>
+									<p>
+										Language(s):{" "}
+										{country.languages.map((lang, i) => (
+											<span key={i}> {lang.name} </span>
+										))}
+									</p>
+									<p>
+										Phone number prefix:{" "}
+										{country.callingCodes.map((code, i) => (
+											<span key={i}> +{code} </span>
+										))}
 									</p>
 								</div>
-								<img
-									src={`${country.flags.svg}`}
-									alt={`flag of ${country.name}`}
-								/>
-								<p>
-									Population:{" "}
-									<span>
-										{<Numeral value={country.population} format={"0.0a"} />}
-									</span>
-								</p>
-								<p>
-									Language(s):{" "}
-									{country.languages.map((lang, i) => (
-										<span key={i}> {lang.name} </span>
-									))}
-								</p>
-							</div>
-						);
-					})}
+							);
+						})
+					)}
 				</main>
 			</div>
 		);
