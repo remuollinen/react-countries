f25bfa738db31977693859eb323da791b80251f4
Remu Ollinen <remu.ollinen@gmail.com>
2021-11-30 11:00:04 +0200

    added weather api call

--- src/components/CountrySingle.js
+++ src/components/CountrySingle.js
@@ -5,19 +5,41 @@ function getCountry(capital) {
 	return axios.get(`https://restcountries.com/v2/capital/${capital}`);
 }
 
+function getWeather(capital) {
+	return axios.get(
+		`https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${process.env.REACT_APP_ACCESS_KEY}`
+	);
+}
+
 class CountrySingle extends Component {
 	state = {
 		country: [],
-		weather: [],
+		weather: {},
 	};
 
-	componentDidMount() {}
+	componentDidMount() {
+		Promise.all([
+			getCountry(this.props.params.capital),
+			getWeather(this.props.params.capital),
+		]).then((res) => {
+			console.log(res[1].data);
+			this.setState({
+				country: res[0].data,
+				weather: res[1].data,
+			});
+		});
+	}
 
 	render() {
 		return (
 			<div className="country-single">
 				<h1>{this.props.params.capital}</h1>
-				<p>Weather in {this.props.params.capital} is probably freezing</p>
+				<p>The temperature in {this.props.params.capital} is degrees.</p>
+				{/* <img
+					className="weather-icon"
+					src={`http://openweathermap.org/img/wn/${this.state.weather.weather[0].icon}@2x.png`}
+					alt={`${this.state.weather.weather.description} weather icon`}
+				/> */}
 			</div>
 		);
 	}
